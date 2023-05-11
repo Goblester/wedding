07d88de5aac8004bc9e28b89621223a34cfe9bda
Goblester <goblin829@gmail.com>
2023-05-11 08:07:57 +0300

    Initial commit

---
+++ src/components/header/Header.svelte
@@ -0,0 +1,20 @@
+<header class="header">
+    <div class="logo"></div>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Events</li>
+            <li>Story</li>
+            <li>Gallery</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+</header>
+
+
+<style>
+    .header {
+        height: 56px;
+        background-color: gold;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Header from "../components/header/Header.svelte";
+</script>
+<Header />
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the docuasdasdmentation</p>
+
+
+<style>
+    :global(*) {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: Roboto, Arial, sans-serif;
+    }
+</style>
